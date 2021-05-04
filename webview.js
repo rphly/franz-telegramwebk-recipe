bfefbb5ea6ab8d42f99e09a5277a448d4cf32de4
rphly <raphaelyeejunkai@gmail.com>
2021-05-04 10:54:37 +0800

    first commit

---
+++ webview.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    let count = 0;
+    const searchElement = document.querySelector('.im_dialogs_search_field');
+    if (searchElement && searchElement.value === '') {
+      const elements = document.querySelectorAll('.im_dialog_badge:not(.ng-hide):not(.im_dialog_badge_muted)');
+      if (elements) {
+        for (let i = 0; i < elements.length; i += 1) {
+          if (elements[i].innerHTML !== 0) {
+            count += 1;
+          }
+        }
+      }
+    }
+
+    Franz.setBadge(count);
+  };
+
+  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  Franz.loop(getMessages);
+};
